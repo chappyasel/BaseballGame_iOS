0bc1544a107907a0edbc59a25d499ce99d1c86b7
ChappyA12 <chappy_asel@hotmail.com>
2015-09-10 21:32:39 -0400

    Relocated all classes to folders to reflect Xcode environment

---
+++ BaseballGame/BaseballGame/View Controllers/League selection/Custom league cell/CustomLeagueTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomLeagueTableViewCell.h
+//  BaseballGame
+//
+//  Created by Chappy Asel on 9/10/15.
+//  Copyright © 2015 CD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomLeagueTableViewCell : UITableViewCell
+
+@end
---
+++ BaseballGame/BaseballGame/View Controllers/League selection/Custom league cell/CustomLeagueTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomLeagueTableViewCell.m
+//  BaseballGame
+//
+//  Created by Chappy Asel on 9/10/15.
+//  Copyright © 2015 CD. All rights reserved.
+//
+
+#import "CustomLeagueTableViewCell.h"
+
+@implementation CustomLeagueTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
