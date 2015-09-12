b20089fd6e4437ea9a7af4f8ca3e63b4b1d17f86
ChappyA12 <chappy_asel@hotmail.com>
2015-09-12 00:19:56 -0400

    Removed XLForm due to complications

---
+++ BaseballGame/BaseballGame/User Interface/Create league/Create league/CreateLeagueViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateLeagueViewController.h
+//  BaseballGame
+//
+//  Created by Chappy Asel on 9/12/15.
+//  Copyright © 2015 CD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateLeagueViewController : UIViewController
+
+@end
